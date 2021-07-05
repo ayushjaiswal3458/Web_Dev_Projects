f40413982bfb6b68a43bf58ec5135e252f047e7e
Ayush Jaiswal <ayushjaiswal3458@gmail.com>
2021-07-05 21:10:07 +0530

    second commit

---
+++ starbucks-ts/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props{
+
+}
+
+const App: React.FC<Props> = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+    
+App.defaultProps{
+
+}    
+
+    
+export default App;
---
+++ starbucks-ts/src/Link.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props{
+
+}
+
+const component: React.FC<Props> = (props) => {
+    return (
+        <div className="" >
+            
+        </div  >
+    );
+};
+    
+component.defaultProps{
+
+}    
+
+    
+export default component;
